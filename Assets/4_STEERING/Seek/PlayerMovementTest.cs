a0e2335a7801d92e3839102a92dcaf2855d0ad62
turmoil2k <unconfigured@null.spigotmc.org>
2020-04-30 20:58:52 +0400

    final

---
+++ Assets/4_STEERING/Seek/PlayerMovementTest.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+
+public class PlayerMovementTest : MonoBehaviour
+{
+    public float playerSpeed;  // 5
+    public float max_velocity; // 25
+    
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.drag = 5;
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.velocity += transform.forward * playerSpeed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity += -transform.right * playerSpeed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.velocity += -transform.forward * playerSpeed;
+        }
+        if (Input.GetKey(KeyCode.D)) 
+        { 
+            rb.velocity += transform.right * playerSpeed;
+        }
+        if (rb.velocity.magnitude >= max_velocity)
+        {
+            rb.velocity = rb.velocity.normalized * max_velocity;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        //Debug
+        Debug.DrawRay(transform.position + (transform.up * 3), rb.velocity, Color.black);
+        Gizmos.color = Color.black;
+        Gizmos.DrawWireSphere(rb.position + rb.velocity + (transform.up * 3), 1);
+    }
+}
